09f3382dc1e2ad96c9406fab58dc0f728219ef4c
Robert Herdzik <robert.herdzik@yahoo.com>
2018-05-05 15:47:09 +0200

    Insta gradient update

--- InstaGradient/InstaGradient.playground/Contents.swift
+++ InstaGradient/InstaGradient.playground/Contents.swift
@@ -3,7 +3,7 @@ import PlaygroundSupport
 
 let viewFrame = CGRect(x: 0, y: 0, width: 300, height: 300)
 let view = UIView(frame: viewFrame)
-view.backgroundColor = .lightGray
+view.backgroundColor = .white
 
 let animation = CABasicAnimation(keyPath: "position.x")
 animation.duration = 1
@@ -13,27 +13,45 @@ animation.isRemovedOnCompletion = false
 animation.repeatCount = Float.infinity
 animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
 
-let gradient = CAGradientLayer()
-gradient.colors = [
-    UIColor.lightGray.cgColor,
-    UIColor.white.cgColor,
-    UIColor.white.cgColor,
-    UIColor.lightGray.cgColor
-]
-gradient.frame = CGRect(x: 0,
-                        y: 0,
-                        width: 2*viewFrame.width,
-                        height: 2*viewFrame.height)
+func addGradient(view: UIView) -> CAGradientLayer {
+    let gradient = CAGradientLayer()
+    gradient.colors = [
+        UIColor.lightGray.cgColor,
+        UIColor.white.cgColor,
+        UIColor.white.cgColor,
+        UIColor.lightGray.cgColor
+    ]
+    gradient.frame = CGRect(x: 0,
+                            y: 0,
+                            width: 2*view.bounds.width,
+                            height: 2*view.bounds.height)
+    
+    gradient.startPoint = CGPoint(x:0.2, y:0.3)
+    gradient.endPoint = CGPoint(x:1, y:0.9)
+    
+    gradient.opacity = 0.4
+    gradient.locations = [0, 0.2, 0.22, 0.4]
+    
+    gradient.add(animation, forKey: "gradient")
+    
+    return gradient
+}
 
-gradient.startPoint = CGPoint(x:0.2, y:0.3)
-gradient.endPoint = CGPoint(x:1, y:1)
 
-gradient.opacity = 0.4
-gradient.locations = [0, 0.2, 0.22, 0.4]
+var label = UIView(frame: CGRect(x: 0, y: 0, width: 270, height: 70))
+label.backgroundColor = UIColor.lightGray
 
-gradient.add(animation, forKey: "gradient")
+var image = UIView(frame: CGRect(x: 0, y: 80, width: 80, height: 70))
+image.backgroundColor = UIColor.lightGray
 
+[label, image].forEach {
+    view.addSubview($0)
+}
 
-view.layer.addSublayer(gradient)
+[label, image].forEach {
+    let gradient = addGradient(view: label)
+    $0.layer.addSublayer(gradient)
+    $0.layer.masksToBounds = true
+}
 
 PlaygroundPage.current.liveView = view
